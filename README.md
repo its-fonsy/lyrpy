38a435c78ffa257cc72b5697ede36ac11b6a6f19
Fonsy <ciabadiale@gmail.com>
2020-05-07 11:11:30 +0200

    Updated the README and create a first test of the full project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Place Holder README
+# Introduction
+`sync-lyric` is a terminal application to display the lyric of a song played on the `mpd server`.
+
+It's based on `Python` and `ncurses` and has the ability to highlight the verse that is being singed.
---
+++ test/test-sync-lyric/sync-lyric.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import argparse
+from ui import *
+
+def main():
+    parser = argparse.ArgumentParser(description='Display sync lyric of the current playing song on mpd server')
+
+    parser.add_argument('-lf', '--lyrics-folder', nargs=1, help="Specify the location of the lyrics folder")
+
+    args = parser.parse_args()
+
+    if args.lyrics_folder:
+        lyrics_folder = args.lyrics_folder[0]
+
+    curses.wrapper(ui)
+
+
+if __name__ == "__main__":
+    main()
