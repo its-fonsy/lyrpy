d859b3f16758cd2edc0f00dae29168f2833b389b
Fonsy <ciabadiale@gmail.com>
2020-05-29 19:28:44 +0200

    now can search the lyrics from the web

--- lyrpy/search.py
+++ lyrpy/search.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 
 import curses
-
 import requests
+from . import lyrics_folder
 from bs4 import BeautifulSoup
 
-artist = ''
-title = ''
 menu = []
 base_url = "https://syair.info"
 
 def search_url(artist, title):
-    # url = "https://syair.info/search?q=bring+me+the+horizon"
-
+    # Generate the url to search in the site
     artist = '+'.join(artist.lower().split())
     title = '+'.join(title.lower().split())
     url = base_url + '/search?q=' + artist + '+' + title
@@ -74,15 +71,38 @@ def syair(artist, title):
         lyric_file.write(lyric)
         lyric_file.close()
 
-def print_lyric(stdscr, lyric):
+
+def print_lyric(stdscr, lyric, current_row):
     stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    y = 4
-    for line in lyric.split('\n'):
-        stdscr.addstr(y, 5, line)
-        y += 1
-        if y > (h - 5):
-            break
+
+    n_rows, n_cols = stdscr.getmaxyx()
+    offset = 3
+
+    lyric_len = len(lyric)
+
+    if lyric_len < n_rows:
+        idx_end = lyric_len - 2*offset
+    else:
+        idx_end = n_rows - 2*offset
+
+        if current_row + idx_end >= lyric_len:
+            current_row = lyric_len - idx_end
+
+    for idx in range(idx_end):
+        stdscr.addstr(idx + offset, offset, lyric[current_row + idx])
+
+
+def print_menu_title(stdscr, artist, title):
+
+    num_rows, num_cols = stdscr.getmaxyx()
+    y = num_rows//2 - len(menu)//2 - 2
+
+    msg = "Result for "
+    song_msg = artist + ' - ' + title
+    len_msg = len(msg) + len(song_msg)
+
+    stdscr.addstr(y, num_cols//2 - len_msg//2, msg)
+    stdscr.addstr(y, num_cols//2 - len_msg//2 + len(msg), song_msg, curses.A_BOLD)
 
 
 def print_menu(stdscr, selected_row_idx):
@@ -100,29 +120,7 @@ def print_menu(stdscr, selected_row_idx):
     stdscr.refresh()
 
 
-def print_menu_title(stdscr):
-
-    num_rows, num_cols = stdscr.getmaxyx()
-    y = num_rows//2 - len(menu)//2 - 2
-
-    msg = "Result for "
-    song_msg = artist + ' - ' + title
-    len_msg = len(msg) + len(song_msg)
-
-    stdscr.addstr(y, num_cols//2 - len_msg//2, msg)
-    stdscr.addstr(y, num_cols//2 - len_msg//2 + len(msg), song_msg, curses.A_BOLD)
-
-
-def print_center(stdscr, text):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    x = w//2 - len(text)//2
-    y = h//2
-    stdscr.addstr(y, x, text)
-    stdscr.refresh()
-
-
-def print_searching_msg(stdscr):
+def print_searching_msg(stdscr, artist, title):
     # Clear the screen
     stdscr.clear()
     num_rows, num_cols = stdscr.getmaxyx()
@@ -134,23 +132,39 @@ def print_searching_msg(stdscr):
     stdscr.addstr(num_rows // 2 - 1 , num_cols//2 - len_msg//2, msg)
     stdscr.addstr(num_rows // 2 - 1 , num_cols//2 - len_msg//2 + len(msg), song_msg, curses.A_BOLD)
 
-def loop(stdscr):
+def print_saving_msg(stdscr):
+    # Clear the screen
+    stdscr.clear()
+    num_rows, num_cols = stdscr.getmaxyx()
+
+    msg = "Saving the lyric to "
+    len_msg = len(msg) + len(lyrics_folder)
+
+    stdscr.addstr(num_rows // 2 - 1 , num_cols//2 - len_msg//2, msg)
+    stdscr.addstr(num_rows // 2 - 1 , num_cols//2 - len_msg//2 + len(msg), lyrics_folder, curses.A_BOLD)
+    conf_msg = 'Confirm? (y/n)'
+    stdscr.addstr(num_rows // 2, num_cols//2 - len(conf_msg)//2, conf_msg)
+
+    stdscr.refresh()
+
+def loop(stdscr, artist, title):
 
     # getch() is blocking by default, with these func. just wait 150ms
-    stdscr.nodelay(1)
-    stdscr.timeout(150)
-    curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     song_selected = False
 
-    print_searching_msg(stdscr)
+    # Clear the screen and display the searching message
+    stdscr.clear()
+    print_searching_msg(stdscr, artist, title)
     stdscr.box()
     stdscr.refresh()
 
-    global menu
+    # Get a list of found lyrics
     url = search_url(artist, title)
     lyrics_list = result_list(url)
 
+    # Create the menu
+    global menu
     menu = []
     current_row = 0
     for lyric in lyrics_list:
@@ -158,52 +172,70 @@ def loop(stdscr):
     menu.append('EXIT')
 
     while(1):
+
         key = stdscr.getch()
 
         num_rows, num_cols = stdscr.getmaxyx()
-        # Clear the screen
-        for i in range(num_rows):
-            stdscr.move(i, 0)
-            stdscr.clrtoeol()
 
-        if (key == curses.KEY_UP or key == ord('k')) and current_row > 0:
-            current_row -= 1
-        elif (key == curses.KEY_DOWN or key == ord('j')) and current_row < len(menu)-1:
-            current_row += 1
-        elif key == curses.KEY_ENTER or key in [10, 13]:
-            # print_center(stdscr, "You selected '{}'".format(menu[current_row]))
-            # print_center(stdscr, current_row)
+        if not song_selected:
+            if (key == curses.KEY_UP or key == ord('k')) and current_row >= 0:
+                current_row -= 1
 
-            song_selected = True
-            href = lyrics_list[current_row][1]
-            lyric = lyric_fetch(href)
+                # If on the first item on the menu, press up to got to the last
+                if current_row < 0:
+                    current_row = len(menu) - 1
 
-            # if user selected last row, exit the program
-            if current_row == len(menu)-1:
-                break
+            elif (key == curses.KEY_DOWN or key == ord('j')) and current_row <= len(menu)-1:
+                current_row += 1
 
-        if song_selected:
-            print_lyric(stdscr, lyric)
-        else:
+                # If on the last item on the menu, press down to got to the first
+                if current_row == len(menu):
+                    current_row = 0
+
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+
+                # if user selected last row, exit the program
+                if current_row == len(menu)-1:
+                    break
+
+                # User has select a menu entry
+                song_selected = True
+                href = lyrics_list[current_row][1]
+                lyric = lyric_fetch(href)
+                lyric = lyric.split('\n')
+                current_row = 0
+
             print_menu(stdscr, current_row)
-            print_menu_title(stdscr)
+            print_menu_title(stdscr, artist, title)
+
+        else:
+            if (key == curses.KEY_UP or key == ord('k')) and current_row > 0:
+                current_row -= 1
+            elif (key == curses.KEY_DOWN or key == ord('j')):
+                if current_row < (len(lyric) - num_rows + 6):
+                    current_row += 1
+
+            elif key == curses.KEY_ENTER or key in [10, 13]:
+                print_saving_msg(stdscr)
+                stdscr.nodelay(0)
+                key = stdscr.getch()
+                stdscr.nodelay(1)
+                if key == ord('y'):
+                    lyric_file = open(lyrics_folder + artist + ' - ' + title + '.lrc', 'w')
+                    for line in lyric:
+                        lyric_file.write(line + '\n')
+                    lyric_file.close()
+                    break
+
+            print_lyric(stdscr, lyric, current_row)
 
         stdscr.box()
         stdscr.refresh()
 
+
         if ( key == ord('q') ):
-            # Press 'q' to exit
-            break
-
-
-
-
-def main(art, tit):
-
-    global artist
-    artist = art
-    global title
-    title = tit
-
-    curses.wrapper(loop)
-
+            if song_selected:
+                song_selected = False
+                current_row = 0
+            else:
+                break
