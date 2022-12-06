3ccbe14cd7474b998eb7ecf5689ed84db3b30f42
its-fonsy <ciabadiale@gmail.com>
2022-12-06 17:00:58 +0100

    fixed crash when the song played with cmus doesn't have tags

--- lyrpy/cmus_client.py
+++ lyrpy/cmus_client.py
@@ -2,12 +2,14 @@ def cmus_get_artist( response ):
     for tag in (response.split('\n')):
         if 'tag artist' in tag:
             return tag[11:].replace('‐', '-')
+    return 'UNKNOWM'
 
 
 def cmus_get_title( response ):
     for tag in (response.split('\n')):
         if 'tag title' in tag:
             return tag[10:].replace('‐', '-')
+    return 'UNTITLED'
 
 
 def cmus_get_time( response ):
